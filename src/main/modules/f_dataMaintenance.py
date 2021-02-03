0d321ffdf20e759f7a7dfa769ea3b2cdd4b4ee3d
Harish <harishitian@gmail.com>
2021-02-03 11:13:05 +0100

    Update f_dataMaintenance.py

--- src/main/modules/f_dataMaintenance.py
+++ src/main/modules/f_dataMaintenance.py
@@ -1,13 +1,13 @@
 '''
-Created on 07.11.2019
-
-@author: pakala
+Copyright (c) 2021-2022 OVGU LIA
+Author: Harish Kumar Pakala
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+This source code may use other Open Source software components (see LICENSE.txt).
 '''
 
-
-def function(saas, *args):
+def function(pyAAS, *args):
     ''' Data Store Maintenance, for every 1 minutes this modules
         takes a copy of the assetDataTable, deletes from the 
         table values and moves the copy to  the cloud databasse
     '''
-    saas.dataStoreManager.assetDataStoreBackup()
+    pyAAS.dataStoreManager.assetDataStoreBackup()
