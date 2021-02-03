ed5d46eeb290002fd6433d46707af14958eccd5f
Harish <harishitian@gmail.com>
2021-02-03 11:15:28 +0100

    Update datastore.py

--- src/main/abstract/datastore.py
+++ src/main/abstract/datastore.py
@@ -1,8 +1,9 @@
 '''
-Created on 17.09.2019
-
-@author: pakala
-'''
+Copyright (c) 2021-2022 OVGU LIA
+Author: Harish Kumar Pakala
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+This source code may use other Open Source software components (see LICENSE.txt).
+''''
 
 
 class DataStore(object):
