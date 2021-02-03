2b71eef21ce1c8ec283111d28347e222206b1c91
Harish <harishitian@gmail.com>
2021-02-03 11:15:04 +0100

    Update assetendpointhandler.py

--- src/main/abstract/assetendpointhandler.py
+++ src/main/abstract/assetendpointhandler.py
@@ -1,7 +1,8 @@
 '''
-Created on 17.09.2019
-
-@author: pakala
+Copyright (c) 2021-2022 OVGU LIA
+Author: Harish Kumar Pakala
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+This source code may use other Open Source software components (see LICENSE.txt).
 '''
 
 import abc
