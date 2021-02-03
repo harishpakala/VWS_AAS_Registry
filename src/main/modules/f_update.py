5b7383cd9bc05a5898f3b7eb86661fd55f21c51b
Harish <harishitian@gmail.com>
2021-02-03 11:13:30 +0100

    Update f_update.py

--- src/main/modules/f_update.py
+++ src/main/modules/f_update.py
@@ -1,6 +1,11 @@
+'''
+Copyright (c) 2021-2022 OVGU LIA
+Author: Harish Kumar Pakala
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+This source code may use other Open Source software components (see LICENSE.txt).
+'''
 
-
-def function(saas, *args):
+def function(pyAAS, *args):
     """Updates all channels referenced by its name in *args"""
     for channel_name in args:
-        saas.channels[channel_name].update()
+        pyAAS.channels[channel_name].update()
